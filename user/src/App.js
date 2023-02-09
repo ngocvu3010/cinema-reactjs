b98accaa058e91368912be51acebdafe94dbc84c
Ngoc Vu <SUN-ASTERISK\vu.thi.ngoc@b120484-lt.sun-asterisk.com>
2023-02-09 10:48:20 +0700

    add login register layout

--- user/src/App.js
+++ user/src/App.js
@@ -1,10 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import './scss/style.scss';
+
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
     </Routes>
   )
 }
